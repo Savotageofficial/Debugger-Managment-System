0ec535fdc62dfb108b586e949e4e41c8d973e92c
palmhillstry <mohamedgamesar4321@gmail.com>
2025-12-11 16:28:15 +0200

    organized Developer.java

--- src/Developer.java
+++ src/Developer.java
@@ -2,61 +2,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-    public class Developer extends User {
+public class Developer extends User {
 
-        private List<Project> assignedProjects = new ArrayList<>();
-        private List<BugReport> assignedBugs = new ArrayList<>();
-
-
-        public Developer(String name, String email, String password) {
-            super(name, email, password);
-            this.userType = "Developer";
-        }
+    private List<Project> assignedProjects = new ArrayList<>();
+    private List<BugReport> assignedBugs = new ArrayList<>();
 
 
-        public List<Project> getAssignedProjects() {
-            return assignedProjects;
-        }
-
-        public List<BugReport> getAssignedBugs() {
-            return assignedBugs;
-        }
+    public Developer(String name, String email, String password) {
+        super(name, email, password);
+        this.userType = "Developer";
+    }
 
 
-        public void assignProject(Project project) {
-            if (project != null && !assignedProjects.contains(project)) {
-                assignedProjects.add(project);
-            }
-        }
+    public List<Project> getAssignedProjects() {
+        return assignedProjects;
+    }
 
-        public void removeProject(Project project) {
-            assignedProjects.remove(project);
-        }
+    public List<BugReport> getAssignedBugs() {
+        return assignedBugs;
+    }
 
 
-        public void assignBug(BugReport bug) {
-            if (bug != null && !assignedBugs.contains(bug)) {
-                assignedBugs.add(bug);
-            }
-        }
-
-        public void removeBug(BugReport bug) {
-            assignedBugs.remove(bug);
-        }
-
-
-        public static List<Developer> showUsers(List<User> users) {
-            List<Developer> devs = new ArrayList<>();
-            if (users == null) return devs;
-
-            for (User u : users) {
-                if (u instanceof Developer) {
-                    devs.add((Developer) u);
-                }
-            }
-            return devs;
+    public void assignProject(Project project) {
+        if (project != null && !assignedProjects.contains(project)) {
+            assignedProjects.add(project);
         }
     }
-}
 
+    public void removeProject(Project project) {
+        assignedProjects.remove(project);
+    }
+
+
+    public void assignBug(BugReport bug) {
+        if (bug != null && !assignedBugs.contains(bug)) {
+            assignedBugs.add(bug);
+        }
+    }
+
+    public void removeBug(BugReport bug) {
+        assignedBugs.remove(bug);
+    }
+
+
+    public static List<Developer> showUsers(List<User> users) {
+        List<Developer> devs = new ArrayList<>();
+        if (users == null) return devs;
+
+        for (User u : users) {
+            if (u instanceof Developer) {
+                devs.add((Developer) u);
+            }
+        }
+        return devs;
+    }
 }
