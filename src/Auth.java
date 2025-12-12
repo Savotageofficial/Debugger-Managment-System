2296af09ec874e0b89cb9988da2cab99b61cc305
palmhillstry <mohamedgamesar4321@gmail.com>
2025-12-12 15:01:40 +0200

    changed filestorage sorting algorithm and primary keys + adapted the new changes to Auth.java

--- src/Auth.java
+++ src/Auth.java
@@ -4,17 +4,21 @@ public class Auth {
 
 
     public static User Login(String email, String password) {
-        email = email.replace(".com", ".txt");
-        List<String> list = FilesStorage.readlines("developer/" + email); // target path to be changed
+//        email = email.replace(".com", ".txt");
+//        List<String> list = FilesStorage.readlines("developer/" + email); // target path to be changed
 
-        if (password.equalsIgnoreCase(list.get(3))) {
-            User u = new Developer(list.get(1), list.get(2), list.get(3), list.get(4));
-            return u;
-        }else {
-            return null;
+        Developer dev = new Developer();
+        List<User> users = dev.getUsers();
+
+        User u = null;
+
+        for(User user : users){
+            if (user.Email.equalsIgnoreCase(email) && user.getPassword().equalsIgnoreCase(password)){
+                u = user;
+                break;
+            }
         }
-
-
+        return u;
 
     }
 }
